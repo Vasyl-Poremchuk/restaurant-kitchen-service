69628c7c200bea5e40eb4a0705a32f1fb6742c1a
Vasyl Poremchuk <nleveryone@gmail.com>
2022-11-11 00:34:45 +0200

    Create forms.py file and add forms

---
+++ restaurant/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+from restaurant.models import Cook, Dish
+
+
+class CookCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Cook
+        fields = UserCreationForm.Meta.fields + (
+            "first_name", "last_name", "years_of_experience",
+        )
+
+
+class CookYearsOfExperienceUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Cook
+        fields = ("years_of_experience",)
+
+
+class DishForm(forms.ModelForm):
+    cooks = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+
+    class Meta:
+        model = Dish
+        fields = "__all__"
