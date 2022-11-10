68f0a7c5233e26f39cdd2cebc3445f47378e487c
Vasyl Poremchuk <nleveryone@gmail.com>
2022-11-10 19:11:29 +0200

    Add list and detail views to the views.py file

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from restaurant.models import Cook, DishType, Dish
 
@@ -15,3 +16,31 @@ def index(request):
     }
 
     return render(request, template_name="restaurant/index.html", context=context)
+
+
+class DishTypeListView(generic.ListView):
+    model = DishType
+    template_name = "restaurant/dish_type_list.html"
+    context_object_name = "dish_type_list"
+    queryset = DishType.objects.all()
+    paginate_by = 5
+
+
+class DishListView(generic.ListView):
+    model = Dish
+    queryset = Dish.objects.select_related("dish_type")
+    paginate_by = 5
+
+
+class DishDetailView(generic.DetailView):
+    model = Dish
+
+
+class CookListView(generic.ListView):
+    model = Cook
+    paginate_by = 5
+
+
+class CookDetailView(generic.DetailView):
+    model = Cook
+    queryset = Cook.objects.prefetch_related("dishes__dish_type")
