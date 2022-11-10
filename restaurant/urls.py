b42aad1f16854f9df5b1bd14ff8adf6d8d34eecd
Vasyl Poremchuk <nleveryone@gmail.com>
2022-11-10 19:16:10 +0200

    Add path to the urls.py file

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,9 +1,34 @@
 from django.urls import path
 
-from restaurant.views import index
+from restaurant.views import index, DishTypeListView, DishListView, DishDetailView, CookListView, CookDetailView
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "dish_types/",
+        DishTypeListView.as_view(),
+        name="dish-type-list",
+    ),
+    path(
+        "dishes/",
+        DishListView.as_view(),
+        name="dish-list",
+    ),
+    path(
+        "dishes/<int:pk>/",
+        DishDetailView.as_view(),
+        name="dish-detail",
+    ),
+    path(
+        "cookers/",
+        CookListView.as_view(),
+        name="cook-list",
+    ),
+    path(
+        "cookers/<int:pk>",
+        CookDetailView.as_view(),
+        name="cook-detail",
+    ),
 ]
 
 app_name = "restaurant"
