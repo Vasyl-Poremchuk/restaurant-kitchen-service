6134f42c7f5c6183d1477598f6a6438c6cd4eda7
Vasyl Poremchuk <nleveryone@gmail.com>
2022-11-10 20:29:08 +0200

    Add decorator login_required and num_visits variable

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,18 +1,24 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views import generic
 
 from restaurant.models import Cook, DishType, Dish
 
 
+@login_required
 def index(request):
     num_cooks = Cook.objects.count()
     num_dish_types = DishType.objects.count()
     num_dishes = Dish.objects.count()
 
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
     context = {
         "num_cooks": num_cooks,
         "num_dish_types": num_dish_types,
         "num_dishes": num_dishes,
+        "num_visits": num_visits + 1,
     }
 
     return render(request, template_name="restaurant/index.html", context=context)
