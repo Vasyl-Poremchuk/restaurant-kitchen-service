aa3b81cd7e14fd08264a484de9c05890710c4546
Vasyl Poremchuk <nleveryone@gmail.com>
2022-11-10 14:49:01 +0200

    Add default and null parameters to the years_of_experience

--- restaurant/models.py
+++ restaurant/models.py
@@ -13,7 +13,7 @@ class DishType(models.Model):
 
 
 class Cook(AbstractUser):
-    years_of_experience = models.IntegerField()
+    years_of_experience = models.IntegerField(default=0, null=True)
 
     class Meta:
         verbose_name = "cook"
